b4823527ab3cd17186c8c0cc530959120f6067d3
Kaleb S <kalebfire@cox.net>
2017-11-29 14:15:54 -0800

    Update NormalDrive.java
    Commented out normal drive for better readability

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDrive/NormalDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDrive/NormalDrive.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class NormalDrive implements MoveableRobot{
 
     DcMotor L,R;
-    float y,x2,deadZone,turnPower,movePower;
+    float y,x2,deadZone,turnPower,movePower; 
 
     double Dr;
     double Cr;
@@ -17,6 +17,10 @@ public class NormalDrive implements MoveableRobot{
 
     //Creation
 
+    /**
+    * Creates a basic normal drive for the robot with set defaults
+    * @param    hardwaremap the hardware map of the robot for initialization
+    */
     public NormalDrive(HardwareMap hardwareMap){
         deadZone = 0;
         turnPower = 1;
@@ -31,6 +35,13 @@ public class NormalDrive implements MoveableRobot{
         init(hardwareMap);
     }
 
+    /**
+    * Creates a normal drive for the robot with variables for the teleop
+    * @param    deadZone    the radius (from 0-1) of the deadzone from the gamepad
+    * @param    turnPower   scaling modifier for the turning power of the robot
+    * @param    movePower   scaling modifier for the translation movement power of the robot
+    * @param    hardwaremap the hardware map of the robot for initialization
+    */
     public NormalDrive(float deadZone,float turnPower,float movePower,HardwareMap hardwareMap){
         this.deadZone = deadZone;
         this.turnPower = turnPower;
@@ -45,6 +56,13 @@ public class NormalDrive implements MoveableRobot{
         init(hardwareMap);
     }
 
+     /**
+    * Creates a normal drive for the robot with variables for the autonomous
+    * @param    robotDiameter   the length, in inches, between 2 diagonal wheels 
+    * @param    wheelDiameter   the diameter, in inches, of the wheels used on the robot
+    * @param    motorControllerSteps    the amount of ticks the motors have in one revolution
+    * @param    hardwaremap the hardware map of the robot for initialization
+    */
     public NormalDrive(double robotDiameter,double wheelDiameter,int motorControllerSteps,HardwareMap hardwareMap){
         deadZone = 0;
         turnPower = 1;
@@ -59,6 +77,10 @@ public class NormalDrive implements MoveableRobot{
         init(hardwareMap);
     }
 
+     /**
+    * Initializes the motors for the robot
+    * @param    hardwaremap the hardware map of the robot for initialization
+    */
     public void init(HardwareMap hardwareMap){
         L = hardwareMap.dcMotor.get("l");
         R = hardwareMap.dcMotor.get("r");
@@ -67,6 +89,13 @@ public class NormalDrive implements MoveableRobot{
 
     //Tele
 
+     /**
+    * moves the robot in teleop using the 2 thumbsticks
+    * Left joystick: forwards and backwards
+    * Right joystick: turning
+    * @param    gamepad1 the first controller being used for the robot
+    * @param    gamepad2 the second controller being used for the robot
+    */
     public void run(Gamepad gamepad1, Gamepad gamepad2){
         y = gamepad1.left_stick_y;
         if(Math.abs(y) < deadZone)y = 0;
@@ -78,6 +107,10 @@ public class NormalDrive implements MoveableRobot{
 
     //Auto
 
+      /**
+    * moves the robot forward a variable amount of inches
+    * @param    inches the distance, in inches, the robot moves
+    */
     public void forward(float inches){
         int pos = (int)(inches * Mstep / Cw);
         L.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -94,6 +127,10 @@ public class NormalDrive implements MoveableRobot{
         R.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
+      /**
+    * moves the robot backward a variable amount of inches
+    * @param    inches the distance, in inches, the robot moves
+    */
     public void backward(float inches){
         int pos = -(int)(inches * Mstep / Cw);
         L.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -111,18 +148,30 @@ public class NormalDrive implements MoveableRobot{
 
     }
 
+      /**
+    * moves the robot left a variable amount of inches
+    * @param    inches the distance, in inches, the robot moves
+    */
     public void left(float inches){
         cClockwise(90);
         forward(inches);
         clockwise(90);
     }
 
+      /**
+    * moves the robot right a variable amount of inches
+    * @param    inches the distance, in inches, the robot moves
+    */
     public void right(float inches){
         clockwise(90);
         forward(inches);
         cClockwise(90);
     }
 
+      /**
+    * turns the robot clockwise a variable amount of degrees
+    * @param    degrees the amount of degrees the robot turns
+    */
     public void clockwise(int degrees){
         int pos = (int)(degrees * Cr * Math.sqrt(2) * Mstep / 360 / Cw);
         L.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -139,6 +188,10 @@ public class NormalDrive implements MoveableRobot{
         R.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
+      /**
+    * turns the robot counter clockwise a variable amount of degrees
+    * @param    degrees the amount of degrees the robot turns
+    */
     public void cClockwise(int degrees){
         int pos = (int)(degrees * Cr * Math.sqrt(2) * Mstep / 360 / Cw);
         L.setMode(DcMotor.RunMode.RUN_TO_POSITION);
