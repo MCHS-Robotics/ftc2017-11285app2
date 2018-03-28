c4de81308e19a8f10515cb3c1a04820f3833b414
student <bigmich@outlook.com>
2018-03-27 19:54:11 -0700

    need to save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -17,32 +17,53 @@ public class Arm {
 
     float[] handPos = {0,1}; //{open,closed}
 
-    float elbowPos = 1000; //maximum movement
+    int elbowPos = 1000; //maximum movement
 
     float wristPos = .5f; //resting
+
+    float[] wristExt = {0,1};
 
     int elbowMovePos;
 
     float elbowPow;
 
+    /**
+     * Creates an arm object to manipulate the noodles
+     * @param map the hardware map of the robot
+     */
     public Arm(HardwareMap map){
         elbow = map.dcMotor.get("elbow");
         wrist = map.servo.get("wrist");
         hand = map.servo.get("hand");
     }
 
+    /**
+     * Open the claw of the robot
+     */
     public void openHand(){
         hand.setPosition(handPos[0]);
     }
 
+    /**
+     * Closes the claw of the robot
+     */
     public void closeHand(){
         hand.setPosition(handPos[1]);
     }
 
+    /**
+     * Sets the power of the elbow of the robot
+     * @param power the power to move the elbow at
+     */
     public void setElbowPower(float power){
         elbow.setPower(power);
     }
 
+    /**
+     * moves the elbow to a certain motor encoder position
+     * @param pos the motor encoder position of the elbow
+     * @param power the power to move the elbow at
+     */
     public void runElbowTo(int pos,float power){
         elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         elbow.setTargetPosition(pos);
@@ -51,13 +72,30 @@ public class Arm {
         elbowPow = power;
     }
 
+    /**
+     * @preconditon runElbowTo has been called
+     * checks if the elbow has reached its target position
+     */
     public void checkElbow(){
+        if(elbow.getCurrentPosition() > elbowPos)
+            runElbowTo(elbowPos,.1f);
+        if(elbow.getCurrentPosition() < 0)
+            runElbowTo(0,.1f);
         if(!elbow.isBusy()){
             elbow.setPower(0);
         }
     }
 
+    /**
+     * @preconditon runElbowTo has been called
+     * checks if the elbow has reached its target position
+     * @param maxError the maximum error allowed when stopping
+     */
     public void checkElbow(int maxError){
+        if(elbow.getCurrentPosition() > elbowPos)
+            runElbowTo(elbowPos,.1f);
+        if(elbow.getCurrentPosition() < 0)
+            runElbowTo(0,.1f);
         if(!elbow.isBusy()){
             if(Math.abs(elbowMovePos-elbow.getCurrentPosition()) > maxError){
                 elbow.setTargetPosition(elbowMovePos);
@@ -68,7 +106,28 @@ public class Arm {
         }
     }
 
+    /**
+     * Moves the wrist to a resting position
+     */
     public void restWrist(){
         wrist.setPosition(wristPos);
+    }
+
+    /**
+     * Moves the wrist up some amount
+     * @param amt the amount the wrist should move up
+     */
+    public void moveWristUp(float amt){
+        if(wrist.getPosition() >= wristExt[1])
+        wrist.setPosition(wrist.getPosition()+amt);
+    }
+
+    /**
+     * Moves the wrist down some amount
+     * @param amt the amount the wrist should move down
+     */
+    public void moveWristDown(float amt){
+        if(wrist.getPosition() <= wristExt[0])
+        wrist.setPosition(wrist.getPosition()-amt);
     }
 }
